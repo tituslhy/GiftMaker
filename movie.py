d584dcd89ba117dc70eb132255c74dce29d81ed9
Titus Lim <tituslhy@gmail.com>
2023-06-24 15:29:09 +0800

    update code

--- movie.py
+++ movie.py
@@ -1,11 +1,28 @@
 #%%
 import os
-os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg"
-from moviepy.editor import *
+from dotenv import load_dotenv
+load_dotenv()
+import ffmpeg
 from typing import List
 
-def stitch_movie(files: List):
-    pass
+def stitch_movie(files_dir: str, outfile_name: str):
+    clips = []
+    for file in os.listdir(files_dir):
+        file_dir = os.path.join(files_dir, file)
+        clips.append(ffmpeg.input(file_dir))
+    try:
+        (
+            ffmpeg
+            .concat(
+                *clips
+                )
+            .output(f"{files_dir}/{outfile_name}.mp4")
+            .run()
+        )
+    except Exception as e:
+        print(e)
+    print("Files merged")
 # %%
 if __name__ == "__main__":
-    pass
+    files_dir = "./Tests"
+    stitch_movie(files_dir, "happybirthday")
