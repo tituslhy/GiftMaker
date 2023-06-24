c58ff40c0469e37e864a3ba86cc75fe030ea7f87
Titus Lim <tituslhy@gmail.com>
2023-06-24 13:32:15 +0800

    initial commit

---
+++ movie.py
@@ -0,0 +1,11 @@
+#%%
+import os
+os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg"
+from moviepy.editor import *
+from typing import List
+
+def stitch_movie(files: List):
+    pass
+# %%
+if __name__ == "__main__":
+    pass
