fe541cd88c2c9b607798d7ec098d5ad3c84c9be5
Titus Lim <tituslhy@gmail.com>
2023-06-29 22:21:30 +0800

    Update code

--- summarise.py
+++ summarise.py
@@ -7,21 +7,23 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.docstore.document import Document
 # from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.prompts import PromptTemplate
-from langchain.chains.summarize import load_summarize_chain
+# from langchain.chains.summarize import load_summarize_chain
+from langchain.chains import LLMChain
 # import textwrap
 
 
-def summarise_text(text: str, setting: str = 'HuggingFace'):
+def LLM_write(text: str, 
+              setting: str = 'HuggingFace'):
     assert setting in ["HuggingFace", "OpenAI"]
     load_dotenv()
     if setting == "HuggingFace":
         os.environ['HUGGINGFACEHUB_API_TOKEN'] = os.environ.get('HUGGINGFACE_API_TOKEN')
-        llm = HuggingFaceHub(repo_id = "google/flan-t5-base",
-                             model_kwargs = {'temperature': 0.95,
-                                             'max_length': 2048})
-        # llm = HuggingFaceHub(repo_id = "tiiuae/falcon-7b",
-        #                      model_kwargs = {'temperature': 0.99,
+        # llm = HuggingFaceHub(repo_id = "google/flan-t5-base",
+        #                      model_kwargs = {'temperature': 0.90,
         #                                      'max_length': 2048})
+        llm = HuggingFaceHub(repo_id = "tiiuae/falcon-7b-instruct",
+                             model_kwargs = {'temperature': 0.7,
+                                             'max_length': 2048})
     elif setting == "OpenAI":
         OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
         llm = OpenAI(temperature=0, openai_api_key = OPENAI_API_KEY)
@@ -29,21 +31,19 @@ def summarise_text(text: str, setting: str = 'HuggingFace'):
     texts = text_splitter.split_text(text)
     docs = [Document(page_content=t) for t in texts]
     
-    prompt_template = """Write a meaningful summary of the following text:
-
-
+    prompt_template = """Write a funny happy birthday message
+    to Shauna from the following text":
         {text}
-
-
-    Summary:"""
+    """
     PROMPT = PromptTemplate(template = prompt_template, input_variables=['text'])
-    chain = load_summarize_chain(llm, chain_type = 'stuff', prompt = PROMPT)
+    # chain = load_summarize_chain(llm, chain_type = 'stuff', prompt = PROMPT)
+    chain = LLMChain(llm = llm, prompt = PROMPT)
     return chain.run(docs)
 
 #%%
 if __name__ == "__main__":
     with open("./Tests/happybirthdayscript.txt", "r") as f:
         text = f.read()
-    summary = summarise_text(text)
+    summary = LLM_write(text)
     print(summary)
 # %%
